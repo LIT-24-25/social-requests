9555552490bddac5e4d76945f0df16b2cc1a5599
UPLAPPY <andrei.melnikov2008@mail.ru>
2024-12-11 23:22:29 +0300

    serializers done

--- first_app/models.py
+++ first_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Complaint(models.Model):
-    name = models.TextField(_MAX_LENGTH = 30)
-    text = models.TextField(_MAX_LENGTH = 200)
+    name = models.TextField()
+    text = models.TextField()
---
+++ first_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ComplaintListCreate, ComplaintDetail
+
+urlpatterns = [
+    path('complaints/', ComplaintListCreate.as_view(), name='complaint-list-create'),
+    path('complaints/<int:pk>/', ComplaintDetail.as_view(), name='complaint-detail'),
+]
