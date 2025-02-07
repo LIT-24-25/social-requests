1f224ead5d8e0658e4f605c0438b0f0f49d2bdf1
maxxxypya <156666325+maxxxypya@users.noreply.github.com>
2025-02-07 21:03:49 +0300

    models modified

--- complaints/models.py
+++ complaints/models.py
@@ -7,3 +7,4 @@ class Complaint(models.Model):
     x = models.IntegerField(default=0)
     y = models.IntegerField(default=0)
     cluster = models.CharField(max_length=100, default='Unnamed Cluster')
+    embedding = models.TextField()
