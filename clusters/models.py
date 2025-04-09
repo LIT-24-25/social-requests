6111d7703f941572613d1583c1a5a88c524adca6
Alex <152197218+Aletavrus@users.noreply.github.com>
2025-04-09 22:33:21 +0300

    feat: t-88: title for auto clusters (#32)

--- clusters/models.py
+++ clusters/models.py
@@ -26,10 +26,11 @@ class Cluster(models.Model):
             complaints = Complaint.objects.filter(cluster=self)
             if not complaints.exists():
                 return "Нет жалоб для анализа"
-            amount = random.randint(10, 30)
+            amount = random.randint(10, 20)
             complaints_list = list(complaints)
             complaints_for_summary = random.sample(complaints_list, min(amount, len(complaints_list)))
             complaints_texts = [f"Жалоба {i + 1}: {c.text}" for i, c in enumerate(complaints_for_summary)]
+            print(f"amount: {amount}")
 
             combined_text = "\n".join(complaints_texts)
             name = "unable to generate name for cluster"
@@ -46,11 +47,18 @@ class Cluster(models.Model):
 
                         {combined_text}
                         """
-
+                counter = 0
                 while len(name.split(' ')) > 5:
                     name = call_gigachat(prompt_title)
+                    counter+=1
+                    if counter > 10:
+                        break
+                counter = 0
                 while len(summary.split(' ')) > 25:
                     summary = call_gigachat(prompt_summary)
+                    counter+=1
+                    if counter > 10:
+                        break
                 return (name, summary)
 
             else:
