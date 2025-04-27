6cd44fa61f246b7ef098c5f59edfbe89e6b99075
Alex <152197218+Aletavrus@users.noreply.github.com>
2025-04-27 13:38:25 +0300

    fix: openrouter models (#35)

--- clusters/mymodels.py
+++ clusters/mymodels.py
@@ -1,5 +1,7 @@
 from gigachat import GigaChat
 from gigachat.models import Chat, Messages, MessagesRole
+from instructor.exceptions import InstructorRetryException
+
 from clusters.instances import openrouter_token, gigachat_token
 from openai import OpenAI
 from pydantic import BaseModel, Field
@@ -14,10 +16,10 @@ def call_openrouter(prompt):
         client = instructor.from_openai(OpenAI(
             base_url="https://openrouter.ai/api/v1",
             api_key=openrouter_token,
-        ))
+        ), mode=instructor.Mode.TOOLS)
         completion = client.chat.completions.create(
             model = "qwen/qwen-plus",
-            extra_body={"models": ["deepseek/deepseek-chat-v3-0324", "qwen/qwen-max"]},
+            extra_body={"models": ["deepseek/deepseek-chat-v3-0324", "qwen/qwen-max"], "provider": {"require_parameters": True}},
             messages=[
                 {
                     "role": "user",
@@ -26,20 +28,17 @@ def call_openrouter(prompt):
             ],
             response_model=OutputFormat,
             temperature=0,
-            max_tokens=1024,
-            timeout=60,
-            max_retries=3
+            max_retries=3,
+            max_tokens=500
         )
-
-        
         # Ensure we have valid name and summary
         name = completion.name if completion.name else 'Unnamed Cluster'
         summary = completion.summary if completion.summary else 'No summary available'
         model_name = completion._raw_response.model
     
         return name, summary, model_name
-    except Exception as e:
-        print(f"Error in call_openrouter: {str(e)}")
+    except InstructorRetryException as e:
+        print(e.last_completion)
 
 def call_gigachat(prompt):
     payload = Chat(
